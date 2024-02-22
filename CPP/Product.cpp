e519f3e265552c3b2613f5888bd4b81b7966be8c
ssamu11 <134787786+ssamu11@users.noreply.github.com>
2024-02-23 00:27:06 +0700

    Update Product.cpp

--- CPP/Product.cpp
+++ CPP/Product.cpp
@@ -1,3 +1,8 @@
+/*Saya Muhammad Rafie Alhabsyi Setiawan NIM 2202400
+mengerjakan soal Latihan Praktikum 2
+dalam mata kuliah DESAIN DAN PEMROGRAMAN BERORIENTASI
+untuk keberkahan-Nya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
 #include <string>
 
 class Product {
